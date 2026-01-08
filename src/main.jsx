ec50e3d2acc4d64dc08e3ba6109de39dd0cc0778
Lareta064 <lareta1@yandex.ru>
2026-01-08 20:36:55 +0400

    fix-hushrouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 // import {Layout} from './Layout';
 import QrCodeGenerator from "./components/QrCodeGenerator/QrCodeGenerator";
@@ -12,8 +12,8 @@ import { Layout } from './Layout';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 
-   <BrowserRouter basename="/react-qrcode">
+   <HashRouter >
       <Layout />
-    </BrowserRouter>
+    </HashRouter>
 
 );
