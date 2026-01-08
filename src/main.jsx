48d808b6238cd418e936cb8e68e34f4717cd6b38
Lareta064 <lareta1@yandex.ru>
2026-01-08 20:23:55 +0400

    fix-path

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ import { Layout } from './Layout';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 
-   <BrowserRouter>
+   <BrowserRouter basename="/react-qrcode">
       <Layout />
     </BrowserRouter>
 
